54c3ccb2ea4ce8296ce57e4c88247952089c5776
Chioma Kamalu <kamaluchioma062@gmail.com>
2026-04-02 16:04:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ app.add_middleware(
         "http://localhost:3001",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:3001",
+        "https://www.dealflowai.site/", 
+        "https://dealflow-front.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
